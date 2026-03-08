18abe040c5098bdfcf7cb3942f2d6ec8bb156a4a
AMER106 <arabamer031@gmail.com>
2026-03-09 02:11:53 +0530

    controlled the input with script to secure the form

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -2,14 +2,19 @@ import { useState } from "react"
 
 export default function Textarea() {
   const [text,setText]=useState("");
+  const handleChnage=(event)=>{
+      let newText=event.currentTarget.value;
+      if(newText.includes("<script/>")){
+        alert("no script tag is allowed")
+        newText=newText.replace("<script/>"," ")
+      }
+      setText(newText);
+      console.log(text);
+    }
   return (
     <textarea className='textarea' placeholder="Enter your text here" 
     value={text}
-    onChange={(event)=>{
-      const newText=event.currentTarget.value;
-      setText(newText);
-      console.log(text);
-    }}>
+    onChange={handleChnage}>
       
     </textarea>
   )
