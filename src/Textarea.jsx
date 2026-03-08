181f12705010347d5267be88f3e9017456d861f1
AMER106 <arabamer031@gmail.com>
2026-03-09 02:19:56 +0530

    handled the error with warning.jsx component

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react"
+import Warning from "./Warning";
 
 export default function Textarea() {
   const [text,setText]=useState("");
+  const[warning,setWarning]=useState(false);
   const handleChnage=(event)=>{
       let newText=event.currentTarget.value;
       if(newText.includes("<script/>")){
-        alert("no script tag is allowed")
+        setWarning(true)
         newText=newText.replace("<script/>"," ")
       }
       setText(newText);
       console.log(text);
     }
-  return (
+return (
+  <>
     <textarea className='textarea' placeholder="Enter your text here" 
     value={text}
     onChange={handleChnage}>
-      
     </textarea>
-  )
+    {
+    warning ? <Warning /> : 'null'
+    }
+    
+  </>
+)
+  
 }
---
+++ src/Warning.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Warning() {
+  return (
+    <p className="warning">
+       No script tag is allowed
+    </p>
+  )
+}
