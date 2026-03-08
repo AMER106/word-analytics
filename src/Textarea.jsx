078a78b1f6b953c1866d7b52fb6aa1440f01b07c
AMER106 <arabamer031@gmail.com>
2026-03-09 02:39:26 +0530

    removed the warning variable to make the component more optimized

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -3,16 +3,16 @@ import Warning from "./Warning";
 
 export default function Textarea() {
   const [text,setText]=useState("");
-  const[warning,setWarning]=useState(false);
+  
   const[showWarning,setShowWarning]=useState()
   const handleChnage=(event)=>{
       let newText=event.currentTarget.value;
       if(newText.includes("<script/>")){
-        setWarning(true)
+        
         setShowWarning("No script tag is allowed")
         newText=newText.replace("<script/>"," ")
       }else if(newText.includes("@")){
-        setWarning(true)
+        
         setShowWarning("No special character is allowed")
         newText=newText.replace("@","")
       }
@@ -26,7 +26,7 @@ return (
     onChange={handleChnage}>
     </textarea>
     {
-    warning ? <Warning warning={showWarning} /> : ''
+     <Warning warning={showWarning} /> 
     }
     
   </>
