5576e85237a9873e240899c2992b098fe17fb862
AMER106 <arabamer031@gmail.com>
2026-03-10 02:00:23 +0530

    extracts constants into a separate file.

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Statistics from "./Statistics";
 import Textarea from "./Textarea";
+import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from "./constants";
 
 
 export default function Container() {
@@ -9,8 +10,9 @@ export default function Container() {
   const stats={
     numberOfCharacters:text.length,
     numberOfWords:text.split(/\s/).filter((word)=> word!=="").length,
-    instagramCharactersLeft:280-text.length,
-    facebookCharactersLeft:2200-text.length,
+    instagramCharactersLeft:INSTAGRAM_MAX_CHARACTERS-text.length,
+
+    facebookCharactersLeft:FACEBOOK_MAX_CHARACTERS-text.length,
   }
   return (
     <main className="container">
