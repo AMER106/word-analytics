d5a03d9db60b4d0deb140273b6fd692f7dc2ea63
AMER106 <arabamer031@gmail.com>
2026-05-05 00:32:33 +0530

    Test Jenkins polling after baseline

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ export default function Footer() {
     <footer className='footer'>
        <small>&copy;Copyright by AMER. All rights reserved.</small>
        <small>Last checked limits:17 days ago</small>
+       <p>this page is mainly focused on counting the number of words in the text.</p>
     </footer>
   )
 }
