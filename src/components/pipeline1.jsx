eefc99d988ea44b486821ba32fd99e2437364d14
AMER106 <arabamer031@gmail.com>
2026-05-05 21:46:57 +0530

    changes in the pipeline1 page

--- src/components/pipeline1.jsx
+++ src/components/pipeline1.jsx
@@ -4,6 +4,11 @@ const pipeline1 = () => {
   return (
     <div>
       <p>new pipeline created</p>
+      <li>
+        <ol>
+          <li>first step</li>
+        </ol>
+      </li>
     </div>
   )
 }
